b36c6e8eba78bda70e2ba1622077f9bfd578b213
willow <willowmoline@gmail.com>
2019-02-19 16:07:20 -0800

    initial readme doc and updated variable names, function names, and param names for readability

---
+++ change-label/README.md
@@ -0,0 +1,4 @@
+If you're using labels to model sprint, you may need to change the label on incomplete work to move it to the next sprint.
+
+This script searches for incomplete work in an existing label and replaces the existing label with a new label.
+
--- change-label/change_label.py
+++ change-label/change_label.py
@@ -4,30 +4,33 @@ import requests
 # Get your token from the local environment variable and prep it for use in the URL
 clubhouse_api_token = '?token=' + os.getenv('CH_API')
 
-
-#The name of the existing label you want to search for.
-existing_label = 'willowlabeltest'
-
-query = {'query': '!is:done label:"' + existing_label + '"', 'page_size': 25}
-
-
-#A list to store each page of search results for processing.
-stories_list = []
-
-#API URL and endpoint references.
+# API URL and endpoint references.
 api_url_base = 'https://api.clubhouse.io/api/beta'
 search_endpoint = '/search/stories'
 stories_endpoint = '/stories'
 
-def search_request(label_query):
+# The name of the existing label you want to search for.
+existing_label = 'Sprint 1'
+
+search_query = {'query': '!is:done label:"' + existing_label + '"', 'page_size': 25}
+
+# The name and hex color for the label you want to add
+new_label = {'name': 'Sprint 2', 'color': '#ff0022'}
+
+# A list to store each page of search results for processing.
+stories_list = []
+
+
+def search_request(query):
     url = api_url_base + search_endpoint + clubhouse_api_token
-    response = requests.get(url, params=label_query)
+    response = requests.get(url, params=query)
 
     if response.status_code != 200:
         print('Status:', response.status_code, 'Problem with the request. Exiting.')
         exit()
 
     return response.json()
+
 
 def paginate(nextdata):
     url = 'https://api.clubhouse.io' + nextdata + '&token=' + os.getenv('CH_API')
@@ -39,26 +42,27 @@ def paginate(nextdata):
 
     return response.json()
 
-def update_story(id, list_of_labels_on_story):
-    url = api_url_base + stories_endpoint + '/' + id + clubhouse_api_token
-    params = {'labels': list_of_labels_on_story}
+
+def change_story_labels(story_id, labels_on_story):
+    url = api_url_base + stories_endpoint + '/' + story_id + clubhouse_api_token
+    params = {'labels': labels_on_story}
     response = requests.put(url, json=params)
     return response.json()
 
-def update_story_labels(results, existing_label):
-    for story in results:
+
+def assess_story_labels(story_results, remove_label, add_label):
+    for story in story_results:
         story_id = str(story['id'])
-        labels = story['labels']
-        list_of_labels_on_story = [new_label]
-        for label in labels:
-            if label['name'] != existing_label:
-                list_of_labels_on_story.append({'name': label['name']})
-        update_story(story_id, list_of_labels_on_story)
+        list_of_labels_on_story = story['labels']
+        list_of_labels_to_keep = [add_label]
+        for label in list_of_labels_on_story:
+            if label['name'] != remove_label:
+                list_of_labels_to_keep.append({'name': label['name']})
+        change_story_labels(story_id, list_of_labels_to_keep)
     return None
 
-#The name and hex color for the label you want to add
-new_label = {'name': name, 'color': hex_color}
-search_results = search_request(query)
+
+search_results = search_request(search_query)
 
 while search_results['next'] is not None:
     stories_list.append(search_results['data'])
@@ -66,5 +70,5 @@ while search_results['next'] is not None:
 else:
     stories_list.append(search_results['data'])
     for results in stories_list:
-         update_story_labels(results, existing_label)
+        assess_story_labels(results, existing_label, new_label)
     print('Stories updated')
