5d47829d6ce4d98ec01a716132881e6f017fe048
Victor Wu <victor@clubhouse.io>
2019-08-23 10:29:15 -0400

    Create sort.js

---
+++ sort-stories-by-due-date/sort.js
@@ -0,0 +1,43 @@
+const rp = require('request-promise')
+const getOptions = {
+  uri: 'https://api.clubhouse.io/api/v2/search/stories',
+  json: true,
+  qs: {
+    token: 'xxx', // API token
+    query: 'project:50 state:500000044' // Target the stories you care about
+  },  
+}
+
+const putOptions = {
+  method: 'PUT',
+  uri: '',
+  qs: {
+    token: '5d5fe756-2081-4e8a-a56a-273c54361322'    
+  },   
+  body: { },
+  json: true
+}
+
+rp(getOptions).then( (searchResults) => { // Get stories using request-promise.
+  const stories = searchResults.data
+  
+  // Sort stories by deadline (due date) field.
+  stories.sort( (a, b) => {
+    if (!a.deadline) return 1 // Put stories with no deadlines later
+    if (!b.deadline) return -1
+    return a.deadline < b.deadline ? -1 : 1
+  })
+
+  // Send priority order updates to Clubhouse. Wait for each call to finish before going to the next one.
+  async function sendPriorityOrderUpdates() {
+    for (let i = 1; i < stories.length; i++) {
+      putOptions.uri = 'https://api.clubhouse.io/api/v2/stories/' + stories[i].id            
+      putOptions.body.after_id = stories[i-1].id
+      await rp(putOptions)
+    }
+  }
+  sendPriorityOrderUpdates()
+  
+}).catch( (error) => {
+  console.log(error)
+})
