59a238cf8930a4b343aa7600b60a7da91d948fcf
Daniel Gregoire <daniel.gregoire@shortcut.com>
2024-03-27 16:57:05 -0400

    Progress toward generating data/states.csv
    Handles the happy path of finding an "Engineering" workflow with state
    names that are the default.
    
    Handles the less happy path of not finding an "Engineering" workflow
    in the user's workspace. In this case, the script prints out
    instructions, the workflows in their workspace with states, and
    instructs them to use the ID of the workflow in their config.json and
    run it again.
    
    Also handles the less happy path of the "Engineering" workflow being
    identified, but one of the states not matching the expected names. In
    this case, similar flow: instructions, all states printed, user
    expected to manually update data/states.csv and run initialize.py
    again to have it validated.
    
    As of this commit, this script prints out a pre-canned message for
    success and 0 of every kind of entity processed.

--- pivotal-import/initialize.py
+++ pivotal-import/initialize.py
@@ -15,6 +15,7 @@ https://www.pivotaltracker.com/help/articles/story_states/
 """
 
 from collections.abc import Mapping
+import csv
 import json
 import logging
 import os
@@ -26,6 +27,11 @@ import requests
 logger = logging.getLogger(__name__)
 # FIXME Make INFO default upon public release
 logging.basicConfig(level=logging.DEBUG)  # Change to INFO or DEBUG as needed
+
+
+def printerr(s):
+    print(s, file=sys.stderr)
+
 
 # API Helpers
 sc_token = os.getenv("SHORTCUT_API_TOKEN")
@@ -62,44 +68,45 @@ def sc_put(path, data={}):
 
 
 # Mapping of Pivotal Tracker story states to Shortcut ones
-pt_states = {
-    "Unscheduled": {"type": "unstarted", "default_workflow_state": "Unscheduled"},
-    "Unstarted": {"type": "unstarted", "default_workflow_state": "Unscheduled"},
-    "Planned": {"type": "unstarted", "default_workflow_state": "Ready for Development"},
-    "Started": {"type": "started", "default_workflow_state": "In Development"},
-    "Finished": {"type": "started", "default_workflow_state": "Ready for Review"},
-    "Rejected": {"type": "done", "default_workflow_state": "Completed"},
-    "Accepted": {"type": "done", "default_workflow_state": "Completed"},
-}
+pt_all_states = [
+    "unscheduled",
+    "unstarted",
+    "planned",
+    "started",
+    "finished",
+    "rejected",
+    "accepted",
+]
+
+
+def print_workflow_tree(workflows):
+    output_lines = []
+    for workflow in workflows:
+        output_lines.append('Workflow {id} : "{name}"'.format_map(workflow))
+        for workflow_state in workflow["states"]:
+            output_lines.append(
+                '    Workflow State {id} : [{type}] "{name}"'.format_map(workflow_state)
+            )
+    printerr("\n".join(output_lines))
 
 
 def default_workflow_id():
     workflow_id = None
     workflows = sc_get("/workflows")
-    output_lines = []
     for workflow in workflows:
         if workflow["name"] == "Engineering":
             workflow_id = workflow["id"]
 
     if workflow_id is None:
-        print(
-            """Failed to find the default Story Workflow in your Shortcut workspace, please:
-  1. Review the Shortcut Story Workflows printed below
+        printerr(
+            """[Problem] Failed to find the default Story Workflow in your Shortcut workspace, please:
+  1. Review the Shortcut Workflows printed below
   2. Copy the numeric ID of your desired Workflow below
   3. Paste it as the "workflow_id" value in your config.json file.
   4. Rerun initialize.py.
-""",
-            file=sys.stderr,
+"""
         )
-        for workflow in workflows:
-            output_lines.append('Workflow {id} : "{name}"'.format_map(workflow))
-            for workflow_state in workflow["states"]:
-                output_lines.append(
-                    '    Worflow State {id} : [{type}] "{name}"'.format_map(
-                        workflow_state
-                    )
-                )
-        print("\n".join(output_lines), file=sys.stderr)
+        print_workflow_tree(workflows)
         return None
     else:
         return workflow_id
@@ -115,10 +122,15 @@ def populate_config():
     try:
         with open("config.json", "x", encoding="utf-8") as f:
             workflow_id = default_workflow_id()
+            data = {
+                "pt_csv_file": "data/pivotal_export.csv",
+                "states_csv_file": "data/states.csv",
+                "users_csv_file": "data/users.csv",
+                "workflow_id": workflow_id,
+            }
+            json.dump(data, f, indent=2)
             if workflow_id is None:
                 sys.exit(1)
-            data = {"workflow_id": default_workflow_id()}
-            json.dump(data, f, indent=2)
             return data
     except FileExistsError:
         logger.debug(
@@ -128,28 +140,156 @@ def populate_config():
             return json.load(f)
 
 
+def populate_states_csv(states_csv_file, workflow_id):
+    """
+    Writes a CSV file mapping Pivotal Tracker's 7 story states to appropriate
+    Shortcut Workflow States in your workspace.
+
+    If an appropriate Shortcut Workflow State cannot be identified,
+    an empty entry will be placed and this script will print out the IDs, types, and
+    names of all the workflow states in your target workflow so that you can manually
+    replace nulls with the appropriate IDs.
+    """
+    try:
+        with open(states_csv_file, "x") as f:
+            pt_state_mapping = pt_state_mapping_for_workflow(workflow_id)
+            unhandled_pt_states = []
+            writer = csv.DictWriter(
+                f, ["pt_state", "shortcut_state_id", "shortcut_state_name"]
+            )
+            writer.writeheader()
+            for pt_state in pt_all_states:
+                mapping = pt_state_mapping[pt_state]
+                if mapping is None:
+                    unhandled_pt_states.append(pt_state)
+                    writer.writerow(
+                        {
+                            "pt_state": pt_state,
+                            "shortcut_state_id": "",
+                            "shortcut_state_name": "",
+                        }
+                    )
+                else:
+                    writer.writerow(mapping)
+            if unhandled_pt_states:
+                exit_unhandled_pt_states(states_csv_file, unhandled_pt_states)
+    except FileExistsError:
+        unhandled_pt_states = []
+        with open(states_csv_file, "r", newline="") as f:
+            rdr = csv.DictReader(f)
+            for row in rdr:
+                if not row["shortcut_state_id"]:
+                    unhandled_pt_states.append(row["pt_state"])
+        if unhandled_pt_states:
+            exit_unhandled_pt_states(states_csv_file, unhandled_pt_states)
+
+
+def exit_unhandled_pt_states(states_csv_file, unhandled_pt_states):
+    msg = "\n  - ".join(unhandled_pt_states)
+    printerr(
+        f"[Problem] These Pivotal Tracker states couldn't be automatically mapped to Shortcut workflow states:\n  - {msg}\n"
+    )
+    printerr(
+        f"""To resolve this, please:
+1. Review the Shortcut Workflow States printed below.
+2. Copy the numeric IDs of Workflow States you want to map to Pivotal states where there are blanks in your {states_csv_file} file.
+3. Save your {states_csv_file} file and rerun initalize.py to validate it.
+"""
+    )
+    workflows = sc_get("/workflows")
+    print_workflow_tree(workflows)
+    sys.exit(1)
+
+
+def pt_state_mapping_for_workflow(workflow_id):
+    workflow = sc_get(f"/workflows/{workflow_id}")
+    pt_state_mapping = {k: None for k in pt_all_states}
+    for wf_state in workflow["states"]:
+        match (wf_state["type"], wf_state["name"]):
+            case ("unstarted", "Unscheduled"):
+                pt_state_mapping["unscheduled"] = {
+                    "pt_state": "unscheduled",
+                    "shortcut_state_id": wf_state["id"],
+                    "shortcut_state_name": "Unscheduled",
+                }
+            case ("unstarted", "Ready for Development"):
+                pt_state_mapping["unstarted"] = {
+                    "pt_state": "unstarted",
+                    "shortcut_state_id": wf_state["id"],
+                    "shortcut_state_name": "Ready for Development",
+                }
+                pt_state_mapping["planned"] = {
+                    "pt_state": "planned",
+                    "shortcut_state_id": wf_state["id"],
+                    "shortcut_state_name": "Ready for Development",
+                }
+            case ("started", "In Development"):
+                pt_state_mapping["started"] = {
+                    "pt_state": "started",
+                    "shortcut_state_id": wf_state["id"],
+                    "shortcut_state_name": "In Development",
+                }
+            case ("started", "Ready for Review"):
+                pt_state_mapping["finished"] = {
+                    "pt_state": "finished",
+                    "shortcut_state_id": wf_state["id"],
+                    "shortcut_state_name": "Ready for Review",
+                }
+            case ("done", "Completed"):
+                pt_state_mapping["rejected"] = {
+                    "pt_state": "rejected",
+                    "shortcut_state_id": wf_state["id"],
+                    "shortcut_state_name": "Completed",
+                }
+                pt_state_mapping["accepted"] = {
+                    "pt_state": "accepted",
+                    "shortcut_state_id": wf_state["id"],
+                    "shortcut_state_name": "Completed",
+                }
+
+    return pt_state_mapping
+
+
 def validate_config(cfg):
     problems = []
     if sc_token is None:
         problems.append(
             "- You must define a SHORTCUT_API_TOKEN environment variable with your Shortcut API token."
         )
+    if not os.path.isfile("data/pivotal_export.csv"):
+        problems.append(
+            "- Your Pivotal Tracker project export must be located at data/pivotal_export.csv"
+        )
     if not isinstance(cfg, Mapping):
         problems.append(
             "- Your config.json file must be a JSON object, please check its formatting."
         )
-        if cfg["workflow_id"] is None:
+    else:
+        if "workflow_id" not in cfg or not cfg["workflow_id"]:
             problems.append(
-                '- Your config.json file needs a "workflow_id" entry whose value is the ID of the Shortcut Story Workflow this importer should use.'
+                '- Your config.json file needs a "workflow_id" entry whose value is the ID of the Shortcut Workflow this importer should use.'
+            )
+        if "pt_csv_file" not in cfg or not cfg["pt_csv_file"]:
+            problems.append(
+                '- Your config.json file needs a "pt_csv_file" entry whose value is the path to your Pivotal Tracker export CSV.'
             )
     if problems:
         msg = "\n".join(problems)
-        print(msg, file=sys.stderr)
+        printerr(f"Problems:\n{msg}")
+        sys.exit(1)
 
 
 def main():
     cfg = populate_config()
     validate_config(cfg)
+    populate_states_csv(cfg["states_csv_file"], cfg["workflow_id"])
+    print(
+        f"""[Success] Pivotal Tracker export and local configuration have been validated.
+          Identified %d epics, %d stories, %d iterations, and %d labels to import.
+
+[Next] Run 'python pivotal_import.py' to see a dry-run or to perform the actual import into Shortcut."""
+        % (0, 0, 0, 0)
+    )
     return 0
 
 
